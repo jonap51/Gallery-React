52ee3a15461acdbb0f070f97edd1ca5511659e96
jonap51 <scilicet.jp@gmail.com>
2025-02-23 20:28:29 -0300

    agrego info inicial al readme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
-
 function App() {
   return (
     <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quam eaque nesciunt, excepturi ab voluptatum obcaecati voluptate praesentium quis aperiam repellat iste dolores fugiat sed eveniet soluta accusamus modi beatae.
     </div>
   );
 }
