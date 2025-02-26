10caed148a9931eaae5b3510861f4693de7d6e6a
jonap51 <scilicet.jp@gmail.com>
2025-02-26 19:16:42 -0300

    uso json local para test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,43 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import './Main.css'
 import { CardView } from '../components/CardView';
+import './Main.css'
 
 const Main = () => {
 
+    const [fotos, setFotos] = useState([])
     useEffect(() => {
         const obtenerDatos = async () => {
             try {
                 const response = await fetch('/JSON/apiLocal.json')
                 const data = await response.json()
+                setFotos(data)
 
-                console.log(data)
             } catch (e) {
                 console.error(e)
             }
-
         };
         obtenerDatos();
 
+
     }, []);
 
-
+    console.log(fotos, 'hola')
 
     return (
         <div>
             <CardView />
+
+            <div className='gallery container'>
+                {fotos.map((ft) => (
+                    <img key={ft.id} src={ft.img} alt={ft.alt} />
+                ))}
+            </div>
+
         </div>
     )
 }
-
-/*<div className='gallery container'>
-
-                {fotos.map(ft => (
-                    <div>
-
-                        <img src={ft.img} alt="guitar player at concert" />
+/*  <div className='gallery container'>
+                {fotos.map((ft) => (
+                    <div key={ft.id}>
+                        <img src={ft.img} alt={ft.alt} />
                     </div>
                 ))}
-
-
             </div>*/
 export { Main }
